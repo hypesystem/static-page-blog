4104cb764df0a74d4410a92f9d205fae58069537
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-04-19 15:33:56 +0200

    Update build script: - build folder structure if it does not exist - copy API from api folder -- the public/ folder is now entirely generated

---
+++ api/save-blog-post.php
@@ -0,0 +1,30 @@
+<?php
+
+require "../../lib/index.php";
+
+if(!isset($_POST)) {
+    apiRespond(400, array("error" => "Requests to this endpoint must be POST request"));
+    exit();
+}
+
+$data = json_decode(file_get_contents("php://input"));
+
+if($data == null) {
+    apiRespond(400, array("error" => "Could not decode posted JSON", "requestData" => $data));
+    exit();
+}
+
+if(!isset($data->title) || !isset($data->content)) {
+    apiRespond(400, array("error" => "Missing argument. Must provide both title and content fields"));
+    exit();
+}
+
+saveBlogPost($data->title, $data->content);
+renderBlogPost($data->title);
+apiRespond(200, array("ok" => "Saved succesfully"));
+
+function saveBlogPost($title, $content) {
+    file_put_contents("../../data/blog-posts/".$title.".md", $content);
+}
+
+?>
--- build.php
+++ build.php
@@ -2,6 +2,10 @@
 
 require "lib/index.php";
 
+buildPublicFolderStructure();
+
+copyApiToPublic();
+
 $views = getViews();
 renderViews($views);
 
@@ -9,6 +13,30 @@ $blogPosts = getBlogPosts();
 renderBlogPosts($blogPosts);
 //renderIndex();
 //renderAdminOverview();
+
+function buildPublicFolderStructure() {
+    if(!file_exists("public")) {
+        mkdir("public");
+    }
+    if(!file_exists("public/_api")) {
+        mkdir("public/_api");
+    }
+    if(!file_exists("public/blog-posts")) {
+        mkdir("public/blog-posts");
+    }
+}
+
+function copyApiToPublic() {
+    $files = scandir("api");
+    $files = array_diff($files, array(".", ".."));
+    foreach($files as $f) {
+        copyApiFileToPublic($f);
+    }
+}
+
+function copyApiFileToPublic($f) {
+    copy("api/".$f, "public/_api/".$f);
+}
 
 function getViews() {
     $files = scandir("views");
