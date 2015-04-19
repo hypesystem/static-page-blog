e5849a0de1a6dde8f03aa33f65dd4b33cee94396
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-04-19 16:55:12 +0200

    Make sure one can add new blog posts

--- views/_layout.php
+++ views/_layout.php
@@ -8,6 +8,7 @@
         <nav>
             <a href="index.htm">Home</a>
             <a href="admin.htm">Admin-overview</a>
+            <a href="edit-blog-post.htm">+ New blog post</a>
         </nav>
         <?php echo $content; ?>
     </body>
